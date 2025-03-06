6d05f7d4ab0d5cd69fe7508eca6e2c8e2f928c32
root <root@Chaos.vqnyp4ukofoerbaypnltfkrkjd.bx.internal.cloudapp.net>
2025-03-06 22:52:38 +0000

    Separted line

--- node-app/index.js
+++ node-app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, this is my Node.js app running in Docker!Executed by Karthik_IBM');
+  res.send('Hello, this is my Node.js app running in Docker!\nExecuted by Karthik_IBM\n');
 });
 
 app.listen(3000, () => {
